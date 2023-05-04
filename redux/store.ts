6f242cd1ce3f78d86ff55ba2254f5394b2bccf04
Nikolaj Bondarenko <nikolaj@MacBook-Air-Nikolaj.local>
2023-05-04 23:03:18 +0600

    complete feature

--- redux/store.ts
+++ redux/store.ts
@@ -1,19 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import formStateReducer from "./slices/formStateSlice";
 import shiftStateReducer from "./slices/shiftStateSlice";
 import cashboxStateReducer from "./slices/cashboxStateSlice";
 import salaryStateReducer from "./slices/salaryStateSlice";
-import startSumStateReducer from './slices/startSumStateSlice'
+import startSumStateReducer from "./slices/startSumStateSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  formState: formStateReducer,
+  shiftState: shiftStateReducer,
+  cashboxState: cashboxStateReducer,
+  salaryState: salaryStateReducer,
+  startSumState: startSumStateReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    formState: formStateReducer,
-    shiftState: shiftStateReducer,
-    cashboxState: cashboxStateReducer,
-    salaryState: salaryStateReducer,
-    startSumState: startSumStateReducer
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
